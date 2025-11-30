9886a46625468215482ccf26f1b399d117a59068
Mark Guinn <mark.guinn@finvari.com>
2025-11-30 13:24:16 -0800

    Shuriken bounces off diagonal walls

--- scenes/Player/shuriken_projectile.gd
+++ scenes/Player/shuriken_projectile.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
 const SPEED = 500.0
+const SLOW_FACTOR = 0.8
 const STOP_SPEED = 250.0
+const RAY_LENGTH = 10.0
 
 @export var velocity: Vector2 = Vector2(500, 0)
 
@@ -9,19 +11,30 @@ const STOP_SPEED = 250.0
 
 func set_direction(dir: Vector2) -> void:
 	velocity = dir * SPEED
-	wall_detector.target_position = dir * 10
+	wall_detector.target_position = dir * RAY_LENGTH
 
 
 func _physics_process(delta: float) -> void:
 	position += velocity * delta
-	
 	if wall_detector.is_colliding():
 		var wall_normal := wall_detector.get_collision_normal()
-		velocity = wall_normal * STOP_SPEED
-		_fade_and_free()
+		var cur_direction := velocity.normalized()
+		var bounce_direction := cur_direction.bounce(wall_normal)
+		var speed := velocity.length()
+
+		print([bounce_direction, wall_normal, cur_direction])
+		if bounce_direction.is_equal_approx(wall_normal):
+			speed = STOP_SPEED
+		else:
+			speed *= SLOW_FACTOR
+		if speed <= STOP_SPEED:
+			_fade_and_free()
+		velocity = bounce_direction * speed
+		wall_detector.target_position = bounce_direction * RAY_LENGTH
+
 
 func _fade_and_free():
 	var tween := create_tween()
-	tween.tween_property(self, "modulate", Color(1.0, 1.0, 1.0, 0.0), 0.5)
+	tween.tween_property(self, "modulate", Color(1.0, 1.0, 1.0, 0.0), 0.4)
 	await tween.finished
 	queue_free()
