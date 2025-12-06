8ff89020749833a0fde6bd3ab31eee80e8973e69
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2025-12-05 20:16:25 -0500

    feat(audio): add mute toggle with M key (behind feature flag)

--- scenes/AudioManager/audio_manager.gd
+++ scenes/AudioManager/audio_manager.gd
@@ -1,6 +1,18 @@
 extends Node
 
 
+func _unhandled_input(event: InputEvent) -> void:
+	if FeatureFlags.is_enabled("mute_toggle"):
+		if event.is_action_pressed("mute"):
+			toggle_mute()
+			get_viewport().set_input_as_handled()
+
+
+func toggle_mute() -> void:
+	var master_bus := AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_mute(master_bus, not AudioServer.is_bus_mute(master_bus))
+
+
 func PlaySFX(audio_stream: AudioStream, target: Node2D) -> AudioStreamPlayback:
 	var freshAudioSource = AudioStreamPlayer2D.new()
 	freshAudioSource.global_position = target.global_position
