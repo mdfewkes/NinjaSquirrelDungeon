5f094765ac5250a8b045542da0cd32082a1b8ebe
Mark Guinn <markguinn@gmail.com>
2025-12-07 12:44:44 -0800

    Make title screen navigable without the mouse

--- scenes/Screens/button_change_scene.gd
+++ scenes/Screens/button_change_scene.gd
@@ -1,9 +1,12 @@
 extends Button
 
 @export var path_to_scene: String = ""
+@export var start_with_focus := false
 
 func _ready() -> void:
 	pressed.connect(on_pressed)
+	if start_with_focus:
+		grab_focus()
 
 func on_pressed() -> void:
 	GameManager.change_scene(path_to_scene)
