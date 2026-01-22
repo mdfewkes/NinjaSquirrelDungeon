84acb97ab3fa2c8aa80f8d8521e64a6f71eeb6d2
Mark Guinn <markguinn@gmail.com>
2026-01-21 19:59:20 -0800

    Cut the slow parts out of the boss cut scene and add optional dialog at the beginning

--- scenes/Boss/IntroCutScene/cut_scene_room.gd
+++ scenes/Boss/IntroCutScene/cut_scene_room.gd
@@ -1,15 +1,24 @@
 extends Room
 
+## If set, this dialog will play before the cut scene starts
+@export var dialog_script_path: String
+
 @onready var anim: AnimationPlayer = $AnimationPlayer
 
 func _start_cut_scene() -> void:
+	if dialog_script_path:
+		DialogueBox.show_dialogue(dialog_script_path, {})
+		await DialogueBox.dialogue_finished
+	
+	# TODO: start music
+	
 	var player: Player = get_tree().get_first_node_in_group("player")
 	player.velocity = Vector2.ZERO
 	player.last_input_vector = Vector2.ZERO
 	player.input_vector = Vector2.ZERO
 	player.is_cutscene_squirrel = true
 	anim.play("enter")
-	
+
 func _shake(amount: float, time: float) -> void:
 	ImpactEffects.hit(amount, time, ImpactEffects.FlashType.Stark, 0.2)
 
