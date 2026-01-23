2fe40b824aefc3e07ec7fd90a88c0d0d8ee2af30
Christer Kaitila <breakdance@mcfunkypants.com>
2026-01-22 16:10:26 -0800

    player footprints do not emit when not moving

---
+++ scenes/Player/footprints.gd
@@ -0,0 +1,14 @@
+extends CPUParticles2D
+
+# footprints using particles!
+
+# this script stops emitting when we are motionless
+# so we don't leave a black spot when standing still
+
+var prevPos = Vector2(-999,-999)
+
+func _process(delta):
+	var dist = prevPos.distance_to(get_parent().position)
+	#print("move dist: "+str(dist))
+	emitting = dist > 0
+	prevPos = get_parent().position
