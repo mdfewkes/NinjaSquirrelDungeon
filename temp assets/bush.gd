826b0a8188fd51cadf45febe92a1f69887e29304
SuperSlickStudios <jakobharris94@gmail.com>
2026-01-24 11:08:56 -0500

    Added Bush Particles to Scene
    Added bush particles to scene, but there is a bug with the particles reemitting over and over.

--- temp assets/bush.gd
+++ temp assets/bush.gd
@@ -1,6 +1,7 @@
 extends StaticBody2D
 
-@export var destroy_effect:PackedScene
+
+@export var bush_particles:PackedScene
 
 @onready var hurt_box: HurtBox = $HurtBox
 
@@ -10,8 +11,8 @@ func _ready() -> void:
 func _on_hurt(hit_box: HitBox):
 	if hit_box.damage <= 0: return
 	
-	if destroy_effect != null:
-		var destroy_effect_inastance = destroy_effect.instantiate()
-		get_tree().current_scene.add_child(destroy_effect_inastance)
-		destroy_effect_inastance.global_position = global_position
-	queue_free()
+	if bush_particles != null:
+		var bush_particles_instance = bush_particles.instantiate()
+		bush_particles_instance.position = global_position
+		get_tree().current_scene.add_child(bush_particles_instance)
+		queue_free()
