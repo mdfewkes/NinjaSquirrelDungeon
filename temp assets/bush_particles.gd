b72b21a4af356db679e29c50e19bbdf41c4a8e9d
Michael Fewkes <mdfewkes@gmail.com>
2026-02-09 22:33:27 -0800

    Destructible crates and barrels

--- temp assets/bush_particles.gd
+++ temp assets/bush_particles.gd
@@ -2,17 +2,5 @@ extends Node2D
 
 @onready var queue_free_timer: Timer = $QueueFreeTimer
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
-
-
 func _on_queue_free_timer_timeout() -> void:
 	queue_free()
-	
